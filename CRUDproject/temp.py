2086bc1bcc85ed766f1172dc9634ba62cb1b7497
Allan Morales Prado <allan.moralesp@utem.cl>
2022-04-22 02:14:15 -0400

    semana santa

--- CRUDproject/temp.py
+++ CRUDproject/temp.py
@@ -17,4 +17,59 @@ def get_list_inmuebles(name,descr):
 
     return lista_inmuebles
 
-resultado = get_list_inmuebles("Providencia","Cocina")
+#resultado = get_list_inmuebles("Providencia","Cocina")
+
+def get_list_inmuebles_by_comuna(comuna):
+    select = f"""
+    SELECT A.id, A.nombre_inmueble, A.descripcion
+    FROM "CRUDapp_inmuebles" as A
+    INNER JOIN "CRUDapp_region" as B
+    ON A.id_region_id = B.id
+    INNER JOIN "CRUDapp_comuna" as C
+    ON A.id_comuna_id = C.id
+    WHERE C.comuna LIKE '%%{str(comuna)}%%'"""
+
+    results = Inmuebles.objects.raw(select)
+
+    archi1 = open("datos.txt","w", encoding='utf-8')
+    for p in results:
+        archi1.write(p.nombre_inmueble + ',' + p.descripcion)
+        archi1.write('\n')
+    archi1.close()
+
+# get_list_inmuebles_by_comuna("Bernardo")
+
+def get_list_inmuebles_region(id):
+    region = str(Region.objects.filter(id=id).values()[0]["region"])
+
+    lista_inmuebles = Inmuebles.objects.filter(id_region_id = id)
+
+    archi1 = open("datos.txt","w", encoding='utf-8')
+    for l in lista_inmuebles.values():
+        archi1.write(str(l["nombre_inmueble"]))
+        archi1.write(',')
+        archi1.write(region)
+        archi1.write("\n")
+    archi1.close()
+
+#get_list_inmuebles_region(16)
+
+def get_list_inmuebles_by_region(region):
+    select = f"""
+    SELECT A.id, A.nombre_inmueble, A.descripcion
+    FROM "CRUDapp_inmuebles" as A
+    INNER JOIN "CRUDapp_region" as B
+    ON A.id_region_id = B.id
+    INNER JOIN "CRUDapp_comuna" as C
+    ON A.id_comuna_id = C.id
+    WHERE B."region" LIKE '%%{str(region)}%%'"""
+
+    results = Inmuebles.objects.raw(select)
+
+    archi1 = open("datos.txt","w", encoding='utf-8')
+    for p in results:
+        archi1.write(p.nombre_inmueble + ',' + p.descripcion)
+        archi1.write('\n')
+    archi1.close()
+
+get_list_inmuebles_by_region("Metrop")
