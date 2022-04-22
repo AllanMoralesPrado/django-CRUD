94eaf48f0987992a749d89e5f00da2eadfb8ed83
Allan Morales Prado <allan.moralesp@utem.cl>
2022-04-22 02:25:33 -0400

    urls

--- CRUDproject/CRUDproject/urls.py
+++ CRUDproject/CRUDproject/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #Se agrega el path hacia las urls de la aplicacion
+    # o es CRUDproject o CRUDapp ya ni me acuerdo
+    path('',include('CRUDapp.urls'))
 ]
